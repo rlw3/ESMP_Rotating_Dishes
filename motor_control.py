273edf45be817f9b7771328761c7037d69aa9dcc
rebecca <rlw3@njit.edu>
2024-08-07 15:35:40 +0200

    control update

--- motor_control.py
+++ motor_control.py
@@ -13,9 +13,9 @@ CCW = 0  # Counter-Clockwise
 SPR = 800  # Steps per revolution in 1/4 step mode
 
 # Pin assignments and setup for the motor
-STEP_PIN = 26
-DIR_PIN = 20
-MODE = (6, 13, 19)
+STEP_PIN = 17
+DIR_PIN = 18
+MODE = (2, 3, 4)
 GPIO.setup([STEP_PIN, DIR_PIN] + list(MODE), GPIO.OUT)
 
 # Set microstepping modes
@@ -36,37 +36,37 @@ try:
     # Get user input for speed and direction
     while True:
         print("Select speed (1-5):")
-        print("1: 0.1 rps")
-        print("2: 0.5 rps")
-        print("3: 1 rps")
-        print("4: 1.5 rps")
-        print("5: 2 rps")
+        # print("1: 0.25 rps")
+        # print("2: 0.5 rps")
+        # print("3: 1 rps")
+        # print("4: 1.5 rps")
+        # print("5: 2 rps")
         speed_option = int(input("Enter speed option: "))
 
         if speed_option == 1:
-            speed = 0.1
-        elif speed_option == 2:
-            speed = 0.5
-        elif speed_option == 3:
             speed = 1
+        elif speed_option == 2:
+            speed = 2.5
+        elif speed_option == 3:
+            speed = 5
         elif speed_option == 4:
-            speed = 1.5
+            speed = 7.5
         elif speed_option == 5:
-            speed = 2
+            speed = 10
+
         else:
             print("Invalid option. Please enter a number between 1 and 5.")
             continue
 
-        direction_input = input("Enter direction (CW/CCW): ").upper()
-        if direction_input == "CW":
+        direction_input = input("Enter direction CW:0 CCW:1: ").upper()
+        if direction_input == "0":
             direction = CW
-        elif direction_input == "CCW":
+        elif direction_input == "1":
             direction = CCW
         else:
             print("Invalid direction. Please enter 'CW' or 'CCW'.")
             continue
 
-        print(f"Motor will run at {speed} rps in {'Clockwise' if direction == CW else 'Counter-Clockwise'} direction.")
         move_motor(speed, direction)
 
 except KeyboardInterrupt:
